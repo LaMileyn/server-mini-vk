1b24abb06926460435337817ec091f0453ef38c0
Volg <lost-is-died@mail.ru>
2022-10-04 18:01:56 +0500

    add update functional

--- controllers/products.controller.js
+++ controllers/products.controller.js
@@ -2,11 +2,24 @@ const db = require("./../settings/db")
 
 class ProductsController {
     async getProducts(req, res) {
-        const data = db.query('SELECT * FROM `shop`', (error, rows, fields) => {
+        db.query('SELECT * FROM `shop`', (error, rows, fields) => {
             if (error) {
                 console.log(error)
             }else{
                res.status(200).json(rows)
+            }
+        })
+    }
+    async buyProduct(req,res) {
+        db.query(`
+        UPDATE shop
+        SET remainder = remainder - 1
+        WHERE id = ${req.params.id}
+        `, (error,rows,fields) =>{
+            if (error){
+                console.log(error)
+            }else{
+                res.status(200).json("success")
             }
         })
     }
--- routes/products.router.js
+++ routes/products.router.js
@@ -2,5 +2,6 @@ const router = require("express").Router();
 const productController = require("./../controllers/products.controller")
 
 router.get("/",productController.getProducts)
+router.put("/:id/buy",productController.buyProduct)
 
 module.exports = router
